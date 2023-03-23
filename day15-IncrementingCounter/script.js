350d8a49aeb32bca23bbecf7e226a65704b2775b
Sarah <sarahvictoria230@gmail.com>
2023-03-23 09:16:07 -0300

    Adding day15-IncrementingCounter

---
+++ day15-IncrementingCounter/script.js
@@ -0,0 +1,20 @@
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        const counterText = +counter.innerText;
+
+        const increment = target / 100
+
+        if (counterText < target) {
+            counter.innerText = `${Math.ceil(counterText + increment)}`;
+            setTimeout(updateCounter, 1);
+        } else {
+            counter.innerText = target
+        }
+    }
+    updateCounter()
+})
